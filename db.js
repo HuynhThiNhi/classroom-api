1376a173b831cebe2966214f4339faa5820cfa5e
huynhthinhi <huynhthinhi206@gmail.com>
2021-10-29 17:59:11 +0700

    fix

--- db.js
+++ db.js
@@ -2,11 +2,11 @@ const sequelize = require('sequelize')
 
 
 const db = new sequelize({
-    database: "classroom",
-    username: "postgres",
-    password: "vuivelavang",
+    database: "de34n1p5p0epc9",
+    username: "hyicjddrryovro",
+    password: "62d5e1cca76bfe4a123ff1ee32494f2eb4cbe54922c30d02b8de96b3b0fb2fff",
 
-    host: "localhost",
+    host: "ec2-44-199-158-170.compute-1.amazonaws.com",
     port: 5432,
     dialect: "postgres",
 	
