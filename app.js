19ee0bfe45ce076e1ede8487b4da2a801a857acf
huynhthinhi <huynhthinhi206@gmail.com>
2021-10-29 10:58:27 +0700

    fix: script start for app

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ app.use('/users', usersRouter);
 app.use('/classes', classRouter)
 
 
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -44,4 +45,9 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`app listening at http://localhost:${port}`)
+})
 module.exports = app;
